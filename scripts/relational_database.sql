4bc99badb76db71e229d83c5aa9cdc8b24cbc7fe
JGray1996 <jamesgray@protonmail.com>
2025-04-15 12:56:04 +0200

    updated database to also accept nrrd files and its segmentation

--- scripts/relational_database.sql
+++ scripts/relational_database.sql
@@ -5,3 +5,12 @@ CREATE TABLE IF NOT EXISTS Dicom (
     placement TEXT NOT NULL,
     time_point TEXT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS Nrrd(
+    file_path TEXT UNIQUE NOT NULL,
+    file_path_segmentation TEXT,
+    vivo TEXT NOT NULL,
+    experiment TEXT NOT NULL,
+    placement TEXT NOT NULL,
+    time_point TEXT NOT NULL
+);
