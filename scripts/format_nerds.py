97519cb269d627fe04ca0bbb54995520e44744ee
jgray1996 <JamesGray@protonmail.com>
2025-06-11 16:08:54 +0200

    Moved these files to the script directory

---
+++ scripts/format_nerds.py
@@ -0,0 +1,14 @@
+from scripts.database import DatabaseHandler
+from scripts.experiments import InputOutput
+from tqdm import tqdm
+
+
+db = DatabaseHandler("databases/dicom.db")
+io = InputOutput()
+all_experiments = db.get_experiments_from_db()
+
+for exp in tqdm(all_experiments):
+    imgs = db.get_images_from_experiment(exp)
+    imgs = io.sort_images(imgs)
+    io.create_nrrd(imgs, exp, "data/processed/nrrds", replace=False)
+
